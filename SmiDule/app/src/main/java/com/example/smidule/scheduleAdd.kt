3399571a388261df348e5e9181288ffee4083d46
harustgump <b1020206@fun.ac.jp>
2021-12-14 15:31:36 +0900

    sample

---
+++ SmiDule/app/src/main/java/com/example/smidule/scheduleAdd.kt
@@ -0,0 +1,11 @@
+package com.example.smidule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class scheduleAdd : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schedule_add)
+    }
+}
